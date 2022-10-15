ce455abe9f55d4bb355a150e5525592fa2d1d14d
Abusev Farid <116abudev@gmail.com>
2022-10-15 17:46:12 +0300

    add pagination and prop types

---
+++ .prettierrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+    trailingComma: "none", // отключаю висящую запятую
+    tabWidth: 4, // размер отступолв аналогично eslitrc.js
+    semi: true // нужна ли точка с запятой
+};
